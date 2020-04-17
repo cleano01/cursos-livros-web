86a234195457b5c9dbddaaadb02d63b33d3304a2
Cleano Ferreira <cleano.ferreira@dce.ufpb.br>
2020-04-17 14:30:11 -0300

    (Design_Patterns_Python_I) primeiro commit

---
+++ Design_Patterns_Python_I/chain_of_responsibility/descontos.py
@@ -0,0 +1,29 @@
+class Desconto__por_cinco_itens:
+
+    def __init__(self, proximo_deesconto):
+        self.__proximo_deesconto = proximo_deesconto
+
+
+    def calcula(self, orcamento):
+        if orcamento.total_itens > 5:
+            return orcamento.valor * 0.1 
+        
+        return self.__proximo_deesconto.calcula(orcamento)
+
+
+class Desconto__por_mais_de_quinhentos_reais:
+
+    def __init__(self, proximo_deesconto):
+        self.__proximo_deesconto = proximo_deesconto
+
+
+    def calcula(self, orcamento):
+        if orcamento.valor > 500:
+            return orcamento.valor * 0.07 
+
+        return self.__proximo_deesconto.calcula(orcamento)
+
+class Sem_desconto:
+
+    def calcula(self, orcamento):
+        return 0
---
+++ Design_Patterns_Python_I/strategy/impostos.py
@@ -0,0 +1,18 @@
+
+class ISS:
+    
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.1
+    '''
+    def calcula_ISS(self, orcamento):
+        return orcamento.valor * 0.1
+    '''
+
+
+class ICMS:
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.06
+    '''
+    def calcula_ICMS(self, orcamento):
+        return orcamento.valor * 0.06
+    '''
