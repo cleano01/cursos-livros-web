56c031250ddd5a0b20226a0adb689e5f39f0c6b0
Cleano Ferreira <cleano.ferreira@dce.ufpb.br>
2020-04-18 17:36:05 -0300

    (decorator_v2)

---
+++ Design_Patterns_Python_I/decorator_v2/calculador_de_imposto.py
@@ -0,0 +1,32 @@
+from impostos  import ICMS, ISS, ICPP, IKCV
+
+class Calculador_de_Impostos:
+
+    def realiza_calculo(self, orcamento, imposto):
+
+        imposto_calculado = imposto.calcula(orcamento)
+        print(imposto_calculado)
+
+    
+if __name__ == '__main__':
+    
+    from orcamento import Orcamento, Item
+
+    calculador = Calculador_de_Impostos()
+
+    orcamento = Orcamento()
+    orcamento.adiciona_item(Item('ITEM-1', 50))
+    orcamento.adiciona_item(Item('ITEM-2', 200))
+    orcamento.adiciona_item(Item('ITEM-3', 250))
+    
+    print('ISS e Icms')
+    calculador.realiza_calculo(orcamento, ISS())
+    calculador.realiza_calculo(orcamento, ICMS())
+    print('composto')
+    calculador.realiza_calculo(orcamento, ISS(ICMS()))
+    
+    print('Icpp e Ikcv')
+    calculador.realiza_calculo(orcamento, ICPP())
+    calculador.realiza_calculo(orcamento, IKCV())
+    print('composto')
+    calculador.realiza_calculo(orcamento, ICPP(IKCV()))
