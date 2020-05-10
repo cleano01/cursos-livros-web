c37b1350f503554c1a615c9a83d8aeca6a462685
cleano01 <cleano.ferreira@dce.ufpb.br>
2020-05-10 16:30:27 -0300

    (sesao7)

---
+++ Programacao_Web_com_Python_e_Django_Framework_ Essencial/secao7/ex2/django2/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.contrib.auth import get_user_model
+
+class Post(models.Model):
+    autor = models.ForeignKey(get_user_model(), verbose_name='Autor', on_delete=models.CASCADE)
+    titulo = models.CharField('Titulo', max_length=50)
+    texto = models.TextField('Texto', max_length=400)
+
+    def __str__(self):
+        return self.titulo
+
+
+    
